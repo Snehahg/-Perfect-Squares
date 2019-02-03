20fce16c28323194639e12f9932b86ac525abfb3
Sneha Hulivan Girisha <34777558+Snehahg@users.noreply.github.com>
2019-02-02 18:11:36 -0600

    Create Perfect-Squares.java

---
+++ Perfect-Squares.java
@@ -0,0 +1,20 @@
+ int[] dp = new int[n+1];
+        int s=0;
+        int dif;
+        for(int i=1;i<=n;i++){
+            dp[i]=Integer.MAX_VALUE;
+        }
+        for(int i=1;i<=n;i++){
+            s=(int)Math.sqrt(i);
+            if(s*s==i){
+                dp[i]=1;
+                continue;
+            }
+            for(int j=1;j<=s;j++){
+                dif= i-j*j;
+                dp[i]=Math.min(dp[i],dp[dif]+1);
+            }
+        }
+        return dp[n];
+    }
+}
